0173a6aaed7030a7759ebafbbef6bbcb81cf252f
Robert Lukotka <robert.lukotka@vacuumlabs.com>
2021-10-13 09:13:07 +0200

    Example app changes

--- example-node/index.ts
+++ example-node/index.ts
@@ -1,43 +1,88 @@
 import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
+import { Fio, HARDENED, GetPublicKeyRequest, SignTransactionRequest, Transaction } from "../src/fio";
+const fetch = require('node-fetch')
+const readline = require('readline');
 
-import { Fio, HARDENED } from "../src/fio";
-
-const getVersion = async (appFio: Fio) => {
-  console.log("getVersion");
-  console.log(await appFio.getVersion());
-  console.log("-".repeat(40));
-}
-
-const getSerial = async (appFio: Fio) => {
-  console.log("getSerial");
-  console.log(await appFio.getSerial());
-  console.log("-".repeat(40));
-}
-
-const getExtendedPublicKey = async (appFio: Fio) => {
-  console.log("getPublicKey");
-  console.log(
-    await appFio.getPublicKey({path: [
-        HARDENED + 44,
-        HARDENED + 235,
-        HARDENED + 0,
-        0,
-        0,
-    ]})
-  );
-  console.log("-".repeat(40));
-}
+const wait = () => {
+  const inputReader = require('wait-console-input')
+  inputReader.wait('');
+}  
 
 
 async function example() {
   console.log("Running FIO examples");
   const transport = await TransportNodeHid.create(5000)
-  // transport.setDebugMode(true)
   const appFio = new Fio(transport);
 
-  await getVersion(appFio);
-  await getSerial(appFio);
-  await getExtendedPublicKey(appFio);
+  console.log("\n".repeat(3));
+  console.log("Input: getVersion");
+  wait()
+  console.log("Response:");
+  console.log(await appFio.getVersion());
+  wait()
+  console.log("\n".repeat(3));
+
+  console.log("Input: getSerial");
+  wait()
+  console.log("Response:");
+  console.log(await appFio.getSerial());
+  wait()
+  console.log("\n".repeat(3));
+
+  const getPublicKeyRequest1: GetPublicKeyRequest = {path: [44 + HARDENED, 235 + HARDENED, 0 + HARDENED, 0, 0]};
+  console.log("Input: getPublicKey");
+  console.log(getPublicKeyRequest1);
+  wait()
+  console.log("Response:");
+  console.log(await appFio.getPublicKey(getPublicKeyRequest1));
+  wait()
+  console.log("\n".repeat(3));
+
+  const getPublicKeyRequest2: GetPublicKeyRequest = {path: [44 + HARDENED, 235 + HARDENED, 0 + HARDENED, 0, 2000]};
+  console.log("Input: getPublicKey");
+  console.log(getPublicKeyRequest2);
+  wait()
+  console.log("Response:");
+  console.log(await appFio.getPublicKey(getPublicKeyRequest2));
+  wait()
+  console.log("\n".repeat(3));
+
+  const basicTx: Transaction = {
+    expiration: "2021-08-28T12:50:36.686",
+    ref_block_num: 0x1122,
+    ref_block_prefix: 0x33445566,
+    context_free_actions: [],
+    actions: [{
+        account: "fio.token",
+        name: "trnsfiopubky",
+        authorization: [{
+            actor: "aftyershcu22",
+            permission: "active",
+        }],
+        data: {
+            payee_public_key: "FIO8PRe4WRZJj5mkem6qVGKyvNFgPsNnjNN6kPhh6EaCpzCVin5Jj",
+            amount: "2000",
+            max_fee: 0x11223344,
+            tpid: "rewards@wallet",
+            actor: "aftyershcu22",
+        },
+    }],
+    transaction_extensions: null,
+  }
+  const infoTestnet = await (await fetch('http://testnet.fioprotocol.io/v1/chain/get_info')).json()
+  const signTransactionRequest: SignTransactionRequest = {
+    path: [44 + HARDENED, 235 + HARDENED, 0 + HARDENED, 0, 0],
+    chainId: infoTestnet.chain_id,
+    tx: basicTx
+  };
+  console.log("Input: signTransaction");
+  console.log(signTransactionRequest);
+  console.log(signTransactionRequest.tx.actions);
+  wait()
+  console.log("Response:");
+  console.log(await appFio.signTransaction(signTransactionRequest));
+  wait()
+  console.log("\n".repeat(3));
 }
 
 example()
